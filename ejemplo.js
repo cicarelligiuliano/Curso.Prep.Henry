9c3ef99ddcac0600aec6fb23ed9a6505031924b5
Giuliano Cicarelli <cicarelligiuliano@outlook.com.ar>
2021-12-02 17:04:22 -0300

    Homework 1 resuelta

---
+++ ejemplo.js
@@ -0,0 +1,16 @@
+function esPositivo(numero) {
+    //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
+    //Si el número es positivo, devolver ---> "Es positivo"
+    //Si el número es negativo, devolver ---> "Es negativo"
+    //Si el número es 0, devuelve false
+    if (numero>0) {
+      return 'Es positivo' 
+    }
+    else if (numero<0) {
+      return 'Es negativo'
+    }
+    else return false
+  }
+esPositivo(2)
+
+
