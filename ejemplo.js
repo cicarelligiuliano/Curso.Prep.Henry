c63b8d50612139217a342e9f1c2235a20e7134a0
Giuliano Cicarelli <cicarelligiuliano@outlook.com.ar>
2021-12-03 13:33:14 -0300

    Ejercicios de unidad 2 Terminados

--- ejemplo.js
+++ ejemplo.js
@@ -1,16 +1,31 @@
-function esPositivo(numero) {
-    //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
-    //Si el número es positivo, devolver ---> "Es positivo"
-    //Si el número es negativo, devolver ---> "Es negativo"
-    //Si el número es 0, devuelve false
-    if (numero>0) {
-      return 'Es positivo' 
-    }
-    else if (numero<0) {
-      return 'Es negativo'
-    }
-    else return false
+function l(f){
+  console.log(f)
+}
+
+function esPrimo(numero) {
+    // Devuelve "true" si "numero" es primo
+    // De lo contrario devuelve "falso"
+    // Pista: un número primo solo es divisible por sí mismo y por 1
+    // Pista 2: Puedes resolverlo usando un bucle `for`
+    // Nota: Los números 0 y 1 NO son considerados números primos
+    if (numero==0 || numero==1){
+      return '0 y 1 no son considerados numeros primos'
+    } 
+    else
+      i=1
+      k=0
+      for (i;i<numero;i++){
+        a=numero%i
+        if (a==0){
+          k++
+        }
+      }
+  if (k>1){
+    return false
+  } else return true
   }
-esPositivo(2)
-
-
+  l(esPrimo(1));
+  l(esPrimo(2));
+  l(esPrimo(3));
+  l(esPrimo(4));
+  l(esPrimo(11));;
